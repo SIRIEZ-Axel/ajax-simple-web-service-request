f481e083efff36aa409492d506f2bd01e95d6ea8
Axel Siriez <axel.siriez@gmail.com>
2022-03-16 10:45:23 +0100

    last push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,21 @@
-let btn = document.getElementById("run")
+
+const quote = document.getElementById("quote")
+const auth = document.getElementById("auth")
+const output = document.getElementById("output")
+
+const btn = document.getElementById("run")
 btn.addEventListener("click", () => {
-    
-    //Create the XHR Object
-    let xhr = new XMLHttpRequest;
-    
-    //Call the open function, GET-type of request, url, true-asynchronous
-    xhr.open('GET', 'https://thatsthespir.it/api', true)
-    
-    //call the onload 
-    xhr.onload = function() {
-        //check if the status is 200(means everything is okay)
-        if (this.status === 200) {
-            const data = JSON.parse(xhr.response);
-            //return server response as an object with JSON.parse
-            console.log(data);
-        }
-    }
-    
-    //call send
-    xhr.send();
 
-    let quote = document.querySelector("#quote")
-
-    quote.src = src.getAttribute('xhr');
-
+    fetch('https://thatsthespir.it/api')
+    .then(res => res.json())
+    .then(data => {
+        quote.textContent = "";
+        output.textContent = "";
+        auth.textContent = "";
+        quote.innerHTML = `${data.quote}`;
+        auth.innerHTML = `${data.author}`;
+        const img = document.createElement("img")
+        img.src = data.photo;
+        output.appendChild(img)
+    })
 })
